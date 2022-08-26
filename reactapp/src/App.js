7714efec3363f87dd8d540f5535238c648d0bc61
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-26 10:55:00 +0300

    init3

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const test = 34;
+const isAuth = true;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Merhaba React Dünyası</h1>
-    
+       {1 + 1}
+       <h4>{"Fulya".toUpperCase()}</h4>
+       <h5>{test}</h5>
+       <div>
+        {
+          isAuth ? <p>Kullanıcı Kayıtlı</p>
+          : null
+
+        }
+       </div>
       </header>
     </div>
+    
   );
 }
 
